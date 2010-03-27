e284d80b8d290a065ea9af8ba1d5bfe507b6ed17
James Halliday <substack@gmail.com>
2010-03-27 15:45:31 -0800

    basic template system

--- frontend/bin/server.py
+++ frontend/bin/server.py
@@ -1,19 +1,41 @@
 #!/usr/bin/env python
 
 from itty import *
+from jinja2 import Environment, FileSystemLoader
+
+class TsunamiApp :
+    def __init__(self,basepath) :
+        self.basepath = basepath
+    
+    def run(self) :
+        run_itty
+        # set up database
+        dbfile = '%s/data/db.sqlite3' % self.basepath
+        
+        # set up jinja2 environment
+        self.env = Environment(loader=FileSystemLoader(
+                '%s/templates' % self.basepath,
+        ))
+        
+        self.layout = self.env.get_template('layout.html')
+        
+        run_itty()
+    
+    def render(self, filename, *args) :
+        template = self.env.get_template(filename)
+        return app.layout.render(content=template.render(*args))
+
+app = None
 
 @get('/')
 def index(web) :
-    return 'Hello!'
-
-@get('/:')
-def moo(web) :
-    return 'default route'
-
+    return str(app.render('index.html'))
+    
 if __name__ == '__main__' :
     import sys, os
     if sys.argv[0] == '' : sys.argv[0] = './'
-    dbfile = '%s/../data/db.sqlite3' % os.path.abspath(
+    basepath = '%s/../' % os.path.abspath(
         os.path.dirname(sys.argv[0])
     )
-    run_itty()
+    app = TsunamiApp(basepath)
+    app.run()
