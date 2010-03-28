ab33cf6c4b7d39f6888c8d65f9c2efc80f65b39c
James Halliday <substack@gmail.com>
2010-03-27 17:04:28 -0800

    objectified database, added deformation table

--- frontend/lib/db.py
+++ frontend/lib/db.py
@@ -1,39 +1,46 @@
-from sqlalchemy import create_engine, \
-    Table, Column, MetaData, ForeignKey, \
-    Integer, Boolean, Float, String
-
-engine = create_engine('sqlite:///db.sqlite', echo=True)
-meta = MetaData()
-
-scenarios = Table('scenarios', meta,
-    Column('id', Integer,  primary_key=True),
-    Column('modeling_time', Float), # hours
-    Column('time_step', Float), # seconds
-    Column('output_step', Float), # seconds
-    Column('sea_level', Float), # meters
-    Column('bottom_friction', Float), # coefficient
-    Column('earth_radius', Float), # meters
-    Column('earth_gravity', Float), # m/(s*s)
-    Column('earth_rotation', Float), # Hz
-    Column('grid', None, ForeignKey('grids.id')),
-)
-
-jobs = Table('jobs', meta,
-    Column('id', Integer,  primary_key=True),
-    Column('scenarios', None, ForeignKey('scenarios.id')),
-    Column('status', String), # pending running complete failed
-)
-
-grids = Table('grids', meta,
-    Column('id', Integer, primary_key=True),
-    Column('name', String)
-)
-
-points = Table('points', meta,
-    Column('id', Integer, primary_key=True),
-    Column('longitude', Float),
-    Column('latitude', Float),
-    Column('grid', None, ForeignKey('grids.id')),
-)
-
-meta.create_all(engine)
+class DB(object) :
+    def __init__(self, filename) :
+        from sqlalchemy import create_engine, \
+            Table, Column, MetaData, ForeignKey, \
+            Integer, Boolean, Float, String
+        
+        self.engine = create_engine('sqlite:///%s' % filename, echo=True)
+        self.meta = MetaData()
+        
+        self.scenarios = Table('scenarios', self.meta,
+            Column('id', Integer,  primary_key=True),
+            Column('modeling_time', Float, nullable=False), # hours
+            Column('time_step', Float, nullable=False), # seconds
+            Column('output_step', Float, nullable=False), # seconds
+            Column('sea_level', Float, nullable=False), # meters
+            Column('bottom_friction', Float, nullable=False), # coefficient
+            Column('earth_radius', Float, nullable=False), # meters
+            Column('earth_gravity', Float, nullable=False), # m/(s*s)
+            Column('earth_rotation', Float, nullable=False), # Hz
+            Column('grid', None, ForeignKey('grids.id')),
+        )
+        
+        self.jobs = Table('jobs', self.meta,
+            Column('id', Integer,  primary_key=True),
+            Column('scenarios', None, ForeignKey('scenarios.id')),
+            Column('status', String, nullable=False), # pending running complete failed
+        )
+        
+        self.grids = Table('grids', self.meta,
+            Column('id', Integer, primary_key=True),
+            Column('name', String, nullable=False)
+        )
+        
+        self.points = Table('points', self.meta,
+            Column('id', Integer, primary_key=True),
+            Column('longitude', Float, nullable=False),
+            Column('latitude', Float, nullable=False),
+            Column('grid', None, ForeignKey('grids.id')),
+        )
+        
+        self.deformations = Table('deformations', self.meta,
+            Column('id', Integer, primary_key=True),
+            Column('name', String, nullable=False),
+        )
+        
+        self.meta.create_all(self.engine)
