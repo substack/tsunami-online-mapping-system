6e4cf77b396a99a97816ec9cfdcf9fb9aafbe27d
James Halliday <substack@gmail.com>
2010-03-27 16:45:52 -0800

    stub of a form

--- frontend/bin/server.py
+++ frontend/bin/server.py
@@ -10,25 +10,35 @@ class TsunamiApp :
     def run(self) :
         run_itty
         # set up database
-        dbfile = '%s/data/db.sqlite3' % self.basepath
+        dbfile = self.root('data/db.sqlite3')
         
         # set up jinja2 environment
         self.env = Environment(loader=FileSystemLoader(
-                '%s/templates' % self.basepath,
+            self.root('templates')
         ))
         
         run_itty()
+    
+    def root(self,*files) :
+        return '%s/%s' % (self.basepath,'/'.join(files))
     
     def render(self, filename, *args, **kwargs) :
         template = self.env.get_template(filename)
         kwargs['layout'] = self.env.get_template(filename)
         return str(template.render(*args, **kwargs))
-
+     
 app = None
 
 @get('/')
-def index(web) :
+def index(request) :
     return app.render('index.html')
+
+@get('/css/(?P<filename>.+)')
+def css(request,filename) :
+    return Response(
+        static_file(filename, root=app.root('css')),
+        content_type=content_type(filename)
+    )
 
 if __name__ == '__main__' :
     import sys, os
