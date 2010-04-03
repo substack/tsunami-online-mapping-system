05ac896ac7b55ebb3364848bd88eec0db4b6e740
James Halliday <substack@gmail.com>
2010-04-02 21:29:10 -0800

    extent types and point normalization

--- frontend/lib/db.py
+++ frontend/lib/db.py
@@ -12,23 +12,31 @@ def bind_db(filename) :
     setup_all()
     if not os.path.exists(filename) : create_all()
 
+class Point(Entity) :
+    longitude = Column(Float)
+    latitude = Column(Float)
+
 class Grid(Entity) :
     name = Column(String)
     points = OneToMany('GridPoint') # one grid for many points
 
-class GridPoint(Entity) :
-    longitude = Column(Float)
-    latitude = Column(Float)
+class GridPoint(Point) :
     grid = ManyToOne('Grid') # many points in one grid
 
 class Deformation(Entity) :
     name = Column(String)
-    points = OneToMany('DeformationPoints') # one deformation for many points
+    points = OneToMany('DeformationPoint') # one deformation for many points
+    extent = ManyToOne('Extent') # many deformations
 
-class DeformationPoints(Entity) :
-    longitude = Column(Float)
-    latitude = Column(Float)
+class DeformationPoint(Point) :
     deformation = ManyToOne('Deformation') # many points in one deformation
+
+class Extent(Entity) :
+    west = Column(Float)
+    south = Column(Float)
+    east = Column(Float)
+    north = Column(Float)
+    deformations = OneToMany('Deformation')
 
 class Scenario(Entity) :
     modeling_time = Column(Float) # hours
