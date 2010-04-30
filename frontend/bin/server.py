c67727359c79abfebbfd3fe7cb25924cfe1517c5
James Halliday <substack@gmail.com>
2010-04-30 01:56:52 -0800

    remove .first() for json() calls since None triggers listing all

--- frontend/bin/server.py
+++ frontend/bin/server.py
@@ -107,7 +107,7 @@ def marker_names(request) :
     return js.dumps([ x[0] for x in session.query(Marker.name).all() ])
 @get('/data/markers/name/(?P<name>.+)')
 def marker_from_name(request, name) :
-    return Marker.json(Marker.query.filter_by(name=name).first())
+    return Marker.json(Marker.query.filter_by(name=name))
 
 # deformations
 @get('/data/deformations')
@@ -118,7 +118,7 @@ def deformation_names(request) :
     return js.dumps([ x[0] for x in session.query(Deformation.name).all() ])
 @get('/data/deformations/name/(?P<name>.+)')
 def deformation_from_name(request, name) :
-    return Deformation.json(Deformation.query.filter_by(name=name).first())
+    return Deformation.json(Deformation.query.filter_by(name=name))
 
 # grids
 @get('/data/grids')
@@ -129,24 +129,31 @@ def grid_names(request) :
     return js.dumps([ x[0] for x in session.query(Grid.name).all() ])
 @get('/data/grids/name/(?P<name>.+)')
 def grid_from_name(request, name) :
-    return Grid.json(Grid.query.filter_by(name=name).first())
+    return Grid.json(Grid.query.filter_by(name=name))
 
 # points
 @get('/data/points')
 def points(request) :
     return Point.json()
-@get('/data/points/names')
-def point_names(request) :
-    return js.dumps([ x[0] for x in session.query(Point.name).all() ])
-@get('/data/points/name/(?P<name>.+)')
-def point_from_name(request, name) :
-    return Point.json(Point.query.filter_by(name=name).first())
+
+# scenarios
+@get('/data/scenarios')
+def scenarios(request) :
+    return Scenario.json()
+@get('/data/scenarios/id/(?P<id>.+)')
+def scenarios(request, id) :
+    return Scenario.json(Scenario.query.filter_by(id=id))
 
 #jobs
+@get('/data/jobs')
+def jobs(request) :
+    return Job.json()
 @get('/data/jobs/pending')
 def pending_jobs(request) :
     return Job.json(Job.query.filter_by(status='pending'))
-
+@get('/data/jobs/pending')
+def pending_jobs(request) :
+    return Job.json(Job.query.filter_by(status='pending'))
 @get('/data/jobs/(?P<job_id>\d+)/update/status/(?P<status>.*)')
 def update_job_status(request,job_id,status) :
     Job.get_by(id=job_id).status = status
