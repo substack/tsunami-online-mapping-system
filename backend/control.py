c9dcf2b658837a5df3a4c21403a0a13cb6638083
James Halliday <substack@gmail.com>
2010-05-04 16:53:08 -0800

    get/set for control module

--- backend/control.py
+++ backend/control.py
@@ -2,10 +2,16 @@ import simplejson as json
 import urllib
 
 class Data(object) :
+    base = "http://localhost:8081/data"
     @classmethod
-    def get(cself,part) :
-        uri = "http://localhost:8081/data/%s/get/%s" % (cself.name(),part)
+    def get(cself,name) :
+        uri = "%s/%s/get/%s" % (cself.base,cself.name(),name)
         return json.loads(urllib.urlopen(uri).read())
+    
+    def set(cself,id,value) :
+        uri = "%s/%s/set/%d/%s" % (cself.base,cself.name(),id,value)
+        ok = urllib.urlopen(uri).read()
+        if ok != "ok" : raise "Response not OK: %s" % ok
     
     @classmethod
     def all(cself) : return cself.get('')
---
+++ backend/crontab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. $HOME/.profile
+module load ncl-5.1.0
+python monitor.py
