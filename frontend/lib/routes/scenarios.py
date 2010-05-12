4b8c01fd14cbdf85f7473cabafdaff6e19ea4e63
James Halliday <substack@gmail.com>
2010-05-11 16:04:27 -0800

    grids, markers routes for scenarios

--- frontend/lib/routes/scenarios.py
+++ frontend/lib/routes/scenarios.py
@@ -1,9 +1,17 @@
 from __common__ import *
 
-@get('/data/scenarios')
+@get('/data/scenarios/get')
 def scenarios(request) :
     return Scenario.json()
 
-@get('/data/scenarios/id/(?P<id>.+)')
-def scenarios(request, id) :
+@get('/data/scenarios/get/id/(?P<sid>.+)')
+def scenarios(request, sid) :
     return Scenario.json(Scenario.query.filter_by(id=id))
+
+@get('/data/scenarios/get/(?P<sid>\d+)/markers')
+def scenarios(request, sid) :
+    return Marker.json(Scenario.get_by(id=sid).markers)
+
+@get('/data/scenarios/get/(?P<sid>\d+)/grids')
+def scenarios(request, sid) :
+    return Marker.json(Scenario.get_by(id=sid).grids)
